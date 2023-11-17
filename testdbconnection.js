ce92388c664b9ea14bcea96ee2a895a85fa4d1a6
Radioactivwaffl <tyler.slagboom@gmail.com>
2023-11-17 14:57:51 -0800

    Update testdbconnection.js
    updated testdbconnection.js to switch to 'tedious' node integration

--- testdbconnection.js
+++ testdbconnection.js
@@ -1,23 +1,32 @@
-// test connection to the database
-// check the "sql server info.txt" 
-// file for valid login data
+// 'Tedious' node is required in order to interface with
+// the MS Azure Sql Server
 
-const mysql = require('mysql2');
+const Connection = require('tedious').Connection
 
-// defines the target connection
-// should keep host: localhost
-// and port: 3306 until further change
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'user',
-    password: 'ValleyTechSystems2007',
-    database: 'items'
+const config = {
+    server: 'localhost',
+    authentication: {
+        // default uses multiple Azur authentification methods
+        type: 'default',
+        options: {
+            // temp names, will need to be filled later
+            userName: 'test',
+            password: 'test'
+        }
+    },
+};
+
+// Turns the Connection object into a connection variable, 
+// with the config settings
+
+const connection = new Connection(congif);
+
+// Create a connection with the variable, not the object
+
+connection.connect((err) => {
+    if (err){
+        console.log('Connection Failed' + err);
+        throw err;
+    }
+    console.log('Connection Successful');
 });
-
-connection.connect(function(error){
-    if (error)
-        return console.error('error: ' + error.message);
-    console.log('Connected');
-});
-
