8281f07bdc57a6d304082fb254eb804b8eab5874
Bejan Maljai <bmaljai@gmail.com>
2023-11-25 14:29:26 -0800

    partial for header added, item page index started, server files updated to handle new request parameters, ejs added and dynamic web page rendering

--- express-api/index.js
+++ express-api/index.js
@@ -9,11 +9,18 @@ import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+import * as path from 'path';
 
 const app = express();
 const PORT = 8080;
 
 app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+// app.use(express.static('public'));
+// Set 'public' directory to serve static files like CSS
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 
 app.use('/tickets',ticketsRoutes);
 app.use('/items',itemsRoutes);
